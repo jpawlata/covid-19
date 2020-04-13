dfd3baa74c18fb6b0d4da15d405031ac7473aa95
Justyna <justyna.pxw@gmail.com>
2020-04-13 15:46:34 +0200

    tests update

--- tests/test.R
+++ tests/test.R
@@ -1,5 +1,6 @@
-app <- ShinyDriver$new("../", loadTimeout = 100000)
+app <- ShinyDriver$new("../", loadTimeout=100000)
 app$snapshotInit("test")
 
 app$snapshot(list(output = "box.sick"))
 app$snapshot(list(output = "box.death"))
+app$snapshot(list(output = "box.recovered"))
